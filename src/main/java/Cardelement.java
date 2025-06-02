681153e54711ad343f779bb63d7242e72e1c896f
SpaceGafa2 <spacegafa2@gmail.com>
2025-06-02 12:55:53 +0200

    Delete src/main directory

--- src/main/java/Cardelement.java
+++
@@ -1,7 +0,0 @@
-public interface Cardelement {
-    public String giveColour();
-    public int giveValue();
-    public boolean giveTrump();
-    public boolean equalss(Cardelement c);
-    public void makeTrump();
-}
