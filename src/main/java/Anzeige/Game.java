17fc3bf287c0b4724591ce4deaf415bd62dbec98
FoggyWatcher <illya.kyslyy@gmail.com>
2025-06-28 14:10:23 +0200

    ein paar änderungen

--- src/main/java/Anzeige/Game.java
+++ src/main/java/Anzeige/Game.java
@@ -12,8 +12,10 @@ import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class Game extends Application{
+public class Game extends Application {
+    //wichtige Variablen
     private Stage primaryStage;
     private HBox bums;
     private HBox kartenleiste;
@@ -25,10 +27,11 @@ public class Game extends Application{
     private Gamers attacker;
     private ArrayList<Cardelement> playerCardsOnHand;
     Label message = new Label("Das Spiel beginnt!");
-    private  boolean gespielt = false;
-    private ArrayList<Cardelement> attackerCardsonTable= new ArrayList<>();
-    private ArrayList<Cardelement> defenderCardsonTable= new ArrayList<>();
-
+    private boolean gespielt = false;
+    private ArrayList<Cardelement> attackerCardsonTable = new ArrayList<>();
+    private ArrayList<Cardelement> defenderCardsonTable = new ArrayList<>();
+    private EndWon endWon;
+    private EndLost endLost;
 
     @Override
     public void start(Stage primStage) {
@@ -42,7 +45,7 @@ public class Game extends Application{
         linkeLeiste = new VBox();
         obereLeiste = new HBox();
 
-        BackgroundImage backImg= new BackgroundImage(new Image(getClass().getResourceAsStream("/BilderProjekt/PokerTable.jpg")),
+        BackgroundImage backImg = new BackgroundImage(new Image(getClass().getResourceAsStream("/BilderProjekt/PokerTable.jpg")),
                 BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
                 BackgroundSize.DEFAULT);
         //"C:\Users\HP\IdeaProjects\GuteInformatikProjekt\target\classes\BilderProjekt\PokerTable.jpg"
@@ -103,6 +106,7 @@ public class Game extends Application{
         //Design der linken leiste
         linkeLeiste.setPrefWidth(120);
         linkeLeiste.setAlignment(Pos.CENTER);
+        linkeLeiste.setSpacing(15);
         //Design der oberen leiste
         obereLeiste.setMaxWidth(Double.MAX_VALUE);
         obereLeiste.setPrefHeight(60);
@@ -113,7 +117,7 @@ public class Game extends Application{
         playTurns();
     }
 
-    public void playTurns(){
+    public void playTurns() {
         //Karten dem Spieler geben
         playerCardsOnHand = spiel.givePlayersCard();
         ArrayList<Button> buttons = new ArrayList<>();
@@ -121,10 +125,10 @@ public class Game extends Application{
         ArrayList<Button> angriffKartenTisch = new ArrayList<>();
 
 
-        for(int i = 0; i < playerCardsOnHand.size(); i++){
+        for (int i = 0; i < playerCardsOnHand.size(); i++) {
             Button butn = new Button();
             butn.setOnAction(null);
-            butn.setId("butn" +(i + 1));
+            butn.setId("butn" + (i + 1));
             //String bildname = playerCardsOnHand.get(i).getImageName();
             //String bildname = "/BilderProjekt/clubsacebetter.png";
             Image bild = new Image(getClass().getResourceAsStream(playerCardsOnHand.get(i).getImageName()));
@@ -140,29 +144,63 @@ public class Game extends Application{
         }
 
         //Deklarieren der Variable für den switch-case
-        int fall = 0;
+        AtomicInteger fall = new AtomicInteger(1);
 
         //Buttons zeigen wie viele Karten gegner haben
         Button gegnerKartenZahl = new Button(String.valueOf(cardNumberEnemy));
         obereLeiste.getChildren().add(gegnerKartenZahl);
 
         //Bestimmen wer anfängt
-        if(spiel.giveBeginner() == null){
+        if (spiel.giveBeginner() == null) {
             //Spieler fängt an, weil er am wenigsten IQ besitzt
             attacker = spiel.givePlayer();
             message.setText("Spieler zieht auf Gegner 1");
-            fall = 1;
-        }else{
+            fall.set(1);
+        } else {
             attacker = spiel.giveBeginner();
-            if(attacker.giveIndex() == 0){
+            if (attacker.giveIndex() == 0) {
                 message.setText("Gegner 1 zieht auf Spieler");
-                fall = 2;
-            }else{
+                fall.set(2);
+            } else {
                 message.setText("Spieler zieht auf Gegner 1");
-                fall = 1;
+                fall.set(1);
             }
         }
 
+        /**
+         * Initialisieren des take-Buttons
+         * Dieser erlaubt es dem Spieler, die karten auf dem Tisch zu nehmen
+         * Die karten werden vom Tisch entfernt und zur Hand des Spielers hinzugefügt
+         */
+        Button takeButton = new Button("TAKE");
+        takeButton.setStyle("-fx-font-size: 16px; -fx-font-weight: bold; -fx-text-fill: black; -fx-background-color: #1d7435; -fx-border-width: 2px; -fx-border-color: #c0e10e");
+        takeButton.setOnAction(e -> {
+            if (attacker != spiel.givePlayer()) {
+                for (int i = 0; i < angriffKartenTisch.size(); i++) {
+                    playerCardsOnHand.add(attackerCardsonTable.get(i));
+                    attackerCardsonTable.remove(attackerCardsonTable.get(i));
+                }
+                for (int i = 0; i < defenderCardsonTable.size(); i++) {
+                    playerCardsOnHand.add(defenderCardsonTable.get(i));
+                    defenderCardsonTable.remove(defenderCardsonTable.get(i));
+                }
+            }
+        });
+        linkeLeiste.getChildren().add(takeButton);
+
+        /**
+         * ich erstelle den "End turn"-Button
+         * dieser wird den Zug des Spielers beenden, wenn er keine weiteren karten legen kann oder will
+         */
+        Button endTurnButton = new Button("End Turn");
+        endTurnButton.setStyle("-fx-font-size: 16px; -fx-font-weight: bold; -fx-text-fill: black; -fx-background-color: #1d7435; -fx-border-width: 2px; -fx-border-color: #c0e10e");
+        endTurnButton.setOnAction(e -> {
+            if (attacker == spiel.givePlayer()) {
+                spiel.endTurn(attacker);
+                fall.set(2);
+            }
+        });
+        linkeLeiste.getChildren().add(endTurnButton);
 
         /**
          * Du dachtest das ist alles?
@@ -171,51 +209,89 @@ public class Game extends Application{
          */
 
         //gefährliches while()
-        while(spiel.givePlayersCard().size() != 0 && spiel.giveCardsNumber(spiel.giveEnemy()) != 0){
-            //hier spielen die Spieler
-            switch(fall){
-                case 1:
-                    //der Spieler zieht gegen gegner1
-                    spiel.setPlaid(attacker);
-                    spiel.setDefender(spiel.giveEnemy());
-                    for(int i = 0; i < buttons.size(); i++){
-                        int finalI = i;
-                        buttons.get(i).setOnAction(e -> {
+        switch (fall.get()) {
+            case 1:
+                //der Spieler zieht gegen gegner1
+                spiel.setPlaid(attacker);
+                spiel.setDefender(spiel.giveEnemy());
+                for (int i = 0; i < buttons.size(); i++) {
+                    int finalI = i;
+                    buttons.get(finalI).setOnAction(e -> {
+                        Cardelement playedCard = playerCardsOnHand.get(finalI);
+                        int cardValue = playerCardsOnHand.get(finalI).giveRealValue();
+                        if (attackerCardsonTable.isEmpty() && defenderCardsonTable.isEmpty()) {
+                            bums.getChildren().add(buttons.get(finalI));
+                            kartenleiste.getChildren().remove(buttons.get(finalI));
+                            attackerCardsonTable.add(playedCard);
+                            playerCardsOnHand.remove(playedCard);
+                        }else if(checkInPlay(cardValue)){
                             bums.getChildren().add(buttons.get(finalI));
                             kartenleiste.getChildren().remove(buttons.get(finalI));
                             angriffKartenTisch.add(buttons.get(finalI));
-                            attackerCardsonTable.add(playerCardsOnHand.get(finalI));
-                            if(checkInPlay(attackerCardsonTable.get(finalI).giveValue())){
-                                buttons.get(finalI).setOnAction(null);
-                            }
-                            gespielt = true;
-                        });
-                    }
+                            attackerCardsonTable.add(playedCard);
+                            playerCardsOnHand.remove(playedCard);
+                        }else{
+                            System.out.println("Andere karte, bitte!");
+                        }
 
-                case 2:
-                    //der gegner zieht gegen den Spieler
-                    spiel.setPlaid(attacker);
-                    spiel.setDefender(spiel.givePlayer());
-                    break;
+
+                    });
+                }
+
+                break;
+
+            case 2:
+                //der gegner zieht gegen den Spieler
+                spiel.setPlaid(attacker);
+                spiel.setDefender(spiel.givePlayer());
+                break;
+
+
+            default:
+                //Endfall, nachdem das Spiel zu ende ist
+                endTheGame();
+        }
+
+    }
+
+    private void endTheGame() {
+        //Hier endet das Spiel anders, je nachdem wer gewonnen hat
+        if (playerCardsOnHand.size() == 0) {
+            endWon = new EndWon();
+            try {
+                endWon.start(new Stage());
+            } catch (Exception exception) {
+                exception.printStackTrace();
             }
+            primaryStage.close();
+        } else {
+            endLost = new EndLost();
+            try {
+                endLost.start(new Stage());
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+            primaryStage.close();
         }
     }
+
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    public boolean checkInPlay(int cardValue){
+    public boolean checkInPlay(int cardValue) {
         boolean check = false;
-        for(int i = 0; i < attackerCardsonTable.size(); i++){
-            if(attackerCardsonTable.get(i).giveValue() == cardValue){
-                check = true;
-            }else if(defenderCardsonTable.get(i).giveValue() == cardValue){
-                check = true;
-            }else{
-                check = false;
+        for (int i = 0; i < attackerCardsonTable.size(); i++) {
+            if (attackerCardsonTable.get(i).giveRealValue() == cardValue) {
+                return true;
             }
         }
-        return check;
+        for (int j = 0; j < defenderCardsonTable.size(); j++) {
+            if (defenderCardsonTable.get(j).giveRealValue() == cardValue) {
+                return true;
+            }
+        }
+        return false;
     }
 }
